17020b023da9f175b78ffed8020476e06fff11b9
ShidaJingTetra <sjing@tetrascience.com>
2021-03-15 22:44:24 -0500

    helloworld in progress

--- ast.mli
+++ ast.mli
@@ -1,18 +1,4 @@
 type expr =
-    Literal of int
-  | Fliteral of string
-  | BoolLit of bool
-  | Id of string
-  | Binop of expr * op * expr
-  | Unop of uop * expr
-  | Assign of string * expr
+    StrLit of string
   | Call of string * expr list
-  | Noexpr
 
-type args_list = None
-type set_of_sets = None
-type tuple_type_size = None
-type enum_matrix_type = None
-type typ = None
-type list_type = None
-type program = None
