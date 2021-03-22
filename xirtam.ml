ea4874e716c7fd0818daee79ed967bf3198d1ded
ShidaJingTetra <jingshid@grinnell.edu>
2021-03-21 20:21:38 -0500

    Made xirtam.ml copy of microC

--- xirtam.ml
+++ xirtam.ml
@@ -1,30 +1,32 @@
+(* Top-level of the MicroC compiler: scan & parse the input,
+   check the resulting AST and generate an SAST from it, generate LLVM IR,
+   and dump the module *)
 
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let expr = Parser.program Scanner.tokenize lexbuf in
-  expr
+type action = Ast | Sast | LLVM_IR | Compile
 
-(* module StringHash = Hashtbl.Make(struct
-      type t = string (* type of keys *)
-      let equal x y = x = y (* use structural comparison *)
-      let hash = Hashtbl.hash (* generic hash function *)
-      end)
-
-let map  = StringHash.create 10
-
-let builtin_print s = s ;;
-
-StringHash.add map "print" builtin_print 
-
-let eval = function
-    StrLit(x) -> 1
-  | Call(v1, v2) -> let temp_f = (StringHash.find map v1) in let s = List.hd v2 in temp_f s ;; *)
-
-(* v1 = "print"
-v2 = ["helloworld"] *)
- 
-(* let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let expr = parserAlt.expr scannerAlt.tokenize lexbuf in
-  let result = eval expr in
-  print_endline (string_of_int result) *)
+let () = 
+  let action = ref Compile in
+  let set_action a () = action := a in
+  let speclist = [
+    ("-a", Arg.Unit (set_action Ast), "Print the AST");
+    ("-s", Arg.Unit (set_action Sast), "Print the SAST");
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
+    ("-c", Arg.Unit (set_action Compile),
+      "Check and print the generated LLVM IR (default)");
+  ] in  
+  let usage_msg = "usage: ./microc.native [-a|-s|-l|-c] [file.mc]" in
+  let channel = ref stdin in
+  Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+  
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Microcparse.program Scanner.token lexbuf in
+  match !action with
+    Ast -> print_string (Ast.string_of_program ast)
+  | _ -> let sast = Semant.check ast in
+    match !action with
+      Ast     -> ()
+    | Sast    -> print_string (Sast.string_of_sprogram sast)
+    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
+    | Compile -> let m = Codegen.translate sast in
+	Llvm_analysis.assert_valid_module m;
+	print_string (Llvm.string_of_llmodule m)
